8562dd47b154c1a11f6a3f2d6cccfdd405cd716c
sara ibnabbad <saraibnabbad96@gmail.com>
2022-12-07 16:55:54 +0100

    add tech

--- src/componant/skills/skill.jsx
+++ src/componant/skills/skill.jsx
@@ -2,7 +2,8 @@ import { Component } from "react";
 import './skill.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faCode} from '@fortawesome/free-solid-svg-icons';
-import { faReact,faHtml5,faCss3Alt,faSquareJs,faBootstrap} from '@fortawesome/free-brands-svg-icons'
+import { faReact,faHtml5,faCss3Alt,faSquareJs,faBootstrap,faphp,falaravel} 
+from '@fortawesomefree-brands-svg-icons';
 import Aos from "aos";
 import "aos/dist/aos.css";
 import { useEffect } from "react";
@@ -24,22 +25,32 @@ const Competence = () => {
 
             <p>CSS</p>
             <div className="skills">
-              <div className="skill css">80%</div>
+              <div className="skill css">90%</div>
             </div>
 
             <p>Bootstrap</p>
             <div className="skills">
-              <div className="skill boot">60%</div>
+              <div className="skill boot">80%</div>
             </div>
 
             <p>JavaScript</p>
             <div className="skills">
-              <div className="skill js">30%</div>
+              <div className="skill js">70%</div>
             </div>
 
             <p>React JS</p>
             <div className="skills">
-              <div className="skill react">10%</div>
+              <div className="skill react">70%</div>
+            </div>
+
+            <p>PHP</p>
+            <div className="skills">
+              <div className="skill react">75%</div>
+            </div>
+
+            <p>Laravel</p>
+            <div className="skills">
+              <div className="skill react">75%</div>
             </div>
           
              </div>
@@ -51,6 +62,8 @@ const Competence = () => {
                 <FontAwesomeIcon icon={faSquareJs} className="iconn"/>
                 <FontAwesomeIcon icon={faReact} className="iconn"/>
                 <FontAwesomeIcon icon={faCode} className="iconn"/>
+                <FontAwesomeIcon icon={faphp}  className="icon"/>
+                <FontAwesomeIcon icon={falaravel} />
             </div>
              
           
